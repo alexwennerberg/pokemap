8a7a86f0ced170b59e1c9dd4b2a99aafeed38a18
alex wennerberg <awennerberg@cloudbakers.com>
2020-02-22 12:04:52 -0600

    Allow turning off frontend, set up basic walking

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::path::Path;
 use mooneye_gb::config::{Bootrom, Cartridge, Model};
 use mooneye_gb::GbKey;
 use mooneye_gb_frontend;
+use std::{thread, time};
 use mooneye_gb_frontend::frontend::{SdlFrontend, InGameState};
 use std::fs::read;
 use std::collections::{HashSet, HashMap};
@@ -159,8 +160,16 @@ impl Game {
 
     }
 
-    fn next_frame(&mut self) {
-        self.frontend.next_frame(&mut self.state).unwrap();
+    fn run_frames(&mut self, mut num: u32, draw: bool) {
+        // headless gives me about 2x speedup
+        for _ in 0..num {
+            if draw {
+                self.frontend.next_frame(&mut self.state, true).unwrap();
+            }
+            else {
+                self.frontend.next_frame(&mut self.state, false).unwrap();
+            }
+        }
     }
 
     fn read_addr(&self, address: u16) -> u8 {
@@ -176,23 +185,45 @@ impl Game {
     /// Navigate beginning of game -- naming characters, etc
     fn navigate_load_screen(&mut self) {
         debug!("Going through startup dialogue with Prof Oak");
-       for _ in 0..150 { 
+       for i in 0..800 { 
             // first iteration -- just smash A for a while
             self.key_down(GbKey::A);
-            self.next_frame();
+            self.run_frames(1, false);
             self.key_up(GbKey::A);
-            self.next_frame();
+            self.run_frames(1, false);
         }
-       for _ in 0..100{
+       for _ in 0..50{
            self.key_down(GbKey::B);
-           self.next_frame();
+           self.run_frames(1, false);
            self.key_up(GbKey::B);
-           self.next_frame();
+           self.run_frames(1, false);
        }
+    }
+    fn walk_randomly(&mut self) {
+        loop {
+            let walk_length = 2;
+            self.key_down(GbKey::Right);
+            self.run_frames(walk_length, true);
+            self.key_up(GbKey::Right);
+            self.run_frames(walk_length, true);
+            self.key_down(GbKey::Down);
+            self.run_frames(walk_length, true);
+            self.key_up(GbKey::Down);
+            self.run_frames(walk_length, true);
+            self.key_down(GbKey::Left);
+            self.run_frames(walk_length, true);
+            self.key_up(GbKey::Left);
+            self.run_frames(walk_length, true);
+            self.key_down(GbKey::Up);
+            self.run_frames(walk_length, true);
+            self.key_up(GbKey::Up);
+            self.run_frames(walk_length, true);
+        }
     }
 
     fn run(&mut self) {
         self.navigate_load_screen();
+        self.walk_randomly();
     }
 }
 
